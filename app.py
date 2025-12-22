f8e70b3682c86f48ed8100eb0f1a52a86190afd1
SHASHANK <shashankdj622@gmail.com>
2025-12-22 14:06:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="ESG Reporting Platform",
     layout="wide",
+    initial_sidebar_state="expanded"
 )
 
 st.title("🌱 ESG Reporting Platform")
